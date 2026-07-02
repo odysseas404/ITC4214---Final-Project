667a53c0a54719d41a2f2006eb2e9a50732f9778
oampatzis <o.ampatzis@acg.edu>
2026-07-02 16:54:29 +0300

    Created a Cameras model to add camera models to the database.

--- catalogue/models.py
+++ catalogue/models.py
@@ -9,3 +9,22 @@ class Category(models.Model):
 
     class Meta:
         verbose_name_plural = "Categories"
+
+class Camera(models.Model):
+    name = models.CharField(max_length=100)
+
+    manufacturer = models.CharField(max_length=100)
+
+    description = models.TextField()
+
+    image = models.ImageField(upload_to="camera_images/")
+
+    available = models.BooleanField(default=True)
+
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return self.name
