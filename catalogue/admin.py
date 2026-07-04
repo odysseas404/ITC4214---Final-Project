5e9d302a9557f278f2d86f6f101ac034311d36fe
oampatzis <o.ampatzis@acg.edu>
2026-07-04 15:57:31 +0300

    Updated catalogue/admin.py in order to add gallery images from the admin panel.

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from .models import Category, Manufacturer, Camera
+from .models import CameraImage, Category, Manufacturer, Camera
 
 admin.site.register(Category)
 admin.site.register(Manufacturer)
 admin.site.register(Camera)
+admin.site.register(CameraImage)
