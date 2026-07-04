7d5b3eb522c3d1e538717e91d66cb191ee7d0773
oampatzis <o.ampatzis@acg.edu>
2026-07-04 14:13:40 +0300

    Created a new Manufacturer model and reorganized the database in order to avoid excess memory consumption through the repetition of Manufacturer names.

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Camera, Category
+
+from .models import Category, Manufacturer, Camera
 
 admin.site.register(Category)
+admin.site.register(Manufacturer)
 admin.site.register(Camera)
--- catalogue/models.py
+++ catalogue/models.py
@@ -10,14 +10,52 @@ class Category(models.Model):
     class Meta:
         verbose_name_plural = "Categories"
 
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["name"]
+
 class Camera(models.Model):
+
+    CONDITION_CHOICES = [
+        ("Excellent", "Excellent"),
+        ("Good", "Good"),
+        ("Fair", "Fair"),
+    ]
+
+    FILM_FORMAT_CHOICES = [
+        ("35mm", "35mm"),
+        ("120", "120"),
+        ("Instant", "Instant"),
+    ]
+
     name = models.CharField(max_length=100)
 
-    manufacturer = models.CharField(max_length=100)
+    manufacturer = models.ForeignKey(
+    Manufacturer,
+    on_delete=models.PROTECT
+    )
+
+    year = models.PositiveIntegerField()
+
+    film_format = models.CharField(
+        max_length=20,
+        choices=FILM_FORMAT_CHOICES
+    )
+
+    condition = models.CharField(
+        max_length=20,
+        choices=CONDITION_CHOICES,
+        default="Excellent"
+    )
 
     description = models.TextField()
 
-    image = models.ImageField(upload_to="camera_images/")
+    main_image = models.ImageField(upload_to="camera_images/")
 
     available = models.BooleanField(default=True)
 
