e9aed90dbad7cd563b6131a0f001a9e8c7c815cc
oampatzis <o.ampatzis@acg.edu>
2026-07-05 13:47:04 +0300

    Updated likes.js file that handles the like button interactivity.

--- catalogue/static/catalogue/js/likes.js
+++ catalogue/static/catalogue/js/likes.js
@@ -1,48 +1,48 @@
-<script>
-    const likeButton = document.getElementById("like-button");
+const likeButton = document.getElementById("like-button");
 
-    if (likeButton) {
-        likeButton.addEventListener("click", function () {
-            const likeUrl = this.dataset.likeUrl;
+if (likeButton) {
+    likeButton.addEventListener("click", function () {
+        const likeUrl = this.dataset.likeUrl;
 
-            fetch(likeUrl, {
-                method: "POST",
-                headers: {
-                    "X-CSRFToken": getCookie("csrftoken"),
-                    "X-Requested-With": "XMLHttpRequest"
-                }
-            })
-            .then(response => response.json())
-            .then(data => {
-                if (data.liked) {
-                    likeButton.textContent = "Unlike";
-                } else {
-                    likeButton.textContent = "Like";
-                }
+        fetch(likeUrl, {
+            method: "POST",
+            headers: {
+                "X-CSRFToken": getCookie("csrftoken"),
+                "X-Requested-With": "XMLHttpRequest"
+            }
+        })
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            if (data.liked) {
+                likeButton.textContent = "Unlike";
+            } else {
+                likeButton.textContent = "Like";
+            }
 
-                document.getElementById("like-count").textContent = data.like_count;
-            });
+            document.getElementById("like-count").textContent = data.like_count;
         });
-    }
+    });
+}
 
-    function getCookie(name) {
-        let cookieValue = null;
+function getCookie(name) {
+    let cookieValue = null;
 
-        if (document.cookie && document.cookie !== "") {
-            const cookies = document.cookie.split(";");
+    if (document.cookie && document.cookie !== "") {
+        const cookies = document.cookie.split(";");
 
-            for (let i = 0; i < cookies.length; i++) {
-                const cookie = cookies[i].trim();
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
 
-                if (cookie.substring(0, name.length + 1) === (name + "=")) {
-                    cookieValue = decodeURIComponent(
-                        cookie.substring(name.length + 1)
-                    );
-                    break;
-                }
+            if (cookie.substring(0, name.length + 1) === (name + "=")) {
+                cookieValue = decodeURIComponent(
+                    cookie.substring(name.length + 1)
+                );
+                break;
             }
         }
-
-        return cookieValue;
     }
-</script>
+
+    return cookieValue;
+}
