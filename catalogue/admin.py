3e0f9631307ffacd082ae63ff06e05cd348ed90b
oampatzis <o.ampatzis@acg.edu>
2026-07-05 14:32:24 +0300

    Admin panel improvement.

--- catalogue/admin.py
+++ catalogue/admin.py
@@ -1,19 +1,71 @@
 from django.contrib import admin
-from .models import Category, Manufacturer, Camera, CameraImage, BorrowRequest, CameraLike 
+from .models import (
+    Category,
+    Manufacturer,
+    Camera,
+    CameraImage,
+    BorrowRequest,
+    CameraLike,
+)
 
 
 class CameraImageInline(admin.TabularInline):
     model = CameraImage
-    extra = 3
+    extra = 1
 
 
 class CameraAdmin(admin.ModelAdmin):
+    list_display = (
+        "name",
+        "manufacturer",
+        "category",
+        "film_format",
+        "condition",
+        "available",
+    )
+
+    list_filter = (
+        "manufacturer",
+        "category",
+        "film_format",
+        "condition",
+        "available",
+    )
+
+    search_fields = (
+        "name",
+        "manufacturer__name",
+    )
+
     inlines = [CameraImageInline]
+
+
+class BorrowRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "camera",
+        "trip_destination",
+        "trip_start_date",
+        "trip_end_date",
+        "status",
+        "created_at",
+    )
+
+    list_filter = (
+        "status",
+        "trip_start_date",
+    )
+
+    search_fields = (
+        "user__username",
+        "camera__name",
+        "trip_destination",
+    )
 
 
 admin.site.register(Category)
 admin.site.register(Manufacturer)
 admin.site.register(Camera, CameraAdmin)
 admin.site.register(CameraImage)
-admin.site.register(BorrowRequest)
+admin.site.register(BorrowRequest, BorrowRequestAdmin)
 admin.site.register(CameraLike)
